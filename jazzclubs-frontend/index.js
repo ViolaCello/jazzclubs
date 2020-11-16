3b0039a28085df202d3eec0c9d96c66fa70146fd
ViolaCello <ViolaCelloNYC@gmail.com>
2020-11-16 10:30:20 -0500

    Create Venue Form info

--- jazzclubs-frontend/index.js
+++ jazzclubs-frontend/index.js
@@ -7,6 +7,29 @@ fetch(`${BACKEND_URL}/clubs`)
   .then(parsedResponse => 
     
     console.log(parsedResponse[0].name, parsedResponse[0].id, parsedResponse[0].location)
- 
-
     );
+
+
+
+    const displayForm = `
+      <form id="createvenue" action="#" data-action="create">
+        <div class="input-field">
+          <input type="text" name="venue" id="venue">
+          <label for="venue">Venue Name</label>
+        </div>
+
+        <div class="input-field">
+          <input type="text" name="location" id="location">
+          <label for="location">Neighborhood</label>
+        </div>
+
+        <div class="input-field">
+        <input type="integer" name="rating" id="rating">
+        <label for="content">Rating (5 = best) </label>
+        <div class="input-field">
+        </div>
+
+        <input id="submit" type="submit" value="Create Venue" class="btn">
+      </form>
+    </div>
+`
