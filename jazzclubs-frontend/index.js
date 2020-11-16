e248c0423b04d64b18126c759af7d448ffff735f
ViolaCello <ViolaCelloNYC@gmail.com>
2020-11-16 12:08:46 -0500

    values for create form working in console

--- jazzclubs-frontend/index.js
+++ jazzclubs-frontend/index.js
@@ -19,14 +19,14 @@ console.log("testing...")
         </div>
 
         <div class="input-field">
-          <input type="text" name="location" id="location">
-          <label for="location">Neighborhood</label>
+          <input type="text" name="nexttime" id="nexttime">
+          <label for="nexttime">Neighborhood</label>
         </div>
 
         <div class="input-field">
         <input type="integer" name="rating" id="rating">
-        <label for="content">Rating (5 = best) </label>
-        <div class="input-field">
+        <label for="rating">Rating (5 = best) </label>
+        
         </div>
 
         <input id="submit" type="submit" value="Create Venue" class="button2">
@@ -37,14 +37,31 @@ console.log("testing...")
 function toDisplayForm() {
   let loc = document.querySelector(".column1")
   loc.innerHTML = displayForm
+
+  const createVenueFormButton = document.querySelector("#submit");
+
+createVenueFormButton.addEventListener("click", function(e) {
+  e.preventDefault()
+  console.log(venue.value, nexttime.value, rating.value)
+  
+  }
+  )
 }
 
 
 // get Form to add a Venue
 const createNewVenueButton = document.querySelector(".button1");
 
-createNewVenueButton .addEventListener("click", function(e) {
+createNewVenueButton.addEventListener("click", function(e) {
   e.preventDefault()
   toDisplayForm()
   }
   )
+
+// const createVenueFormButton = document.querySelector("#submit");
+
+// createVenueFormButton.addEventListener("click", function(e) {
+//   e.preventDefault()
+//   console.log(e)
+//   }
+//   )
