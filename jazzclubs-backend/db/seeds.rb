a8c086e285f7b37ed948779c520651d8cdac914f
ViolaCello <ViolaCelloNYC@gmail.com>
2020-11-24 10:32:33 -0500

    Added seed data to make it easier for people to load and check-out

--- jazzclubs-backend/db/seeds.rb
+++ jazzclubs-backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Review.delete_all
+Club.delete_all
+
+
+club = Club.create(name: "Silvana", location: "Harlem", cover: 15, website: "https://silvana-nyc.com/")
+review1 = Review.create(stars: 5, comments: "Great food upstairs, best raw jazz talent jamming all night downstairs!")
+review2 = Review.create(stars: 4, comments: "The musicians are great, but why does the sound board operator mic the bass drum so loud?")
+
+club.reviews << review1
+club.reviews << review2
